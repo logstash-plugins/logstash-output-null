640e492830a50207ab233c6d4065c68e90867f67
Pier-Hugues Pellerin <phpellerin@gmail.com>
2015-08-28 11:58:02 -0400

    Adding a minimal test suite to test the 2 methods
    Fixes #1
    
    Fixes #2

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# 1.0.1
+- add minimal test coverage
+- add a runtime dependencies on `logstash-codec-plain`
--- spec/outputs/null_spec.rb
+++ spec/outputs/null_spec.rb
@@ -1,7 +1,21 @@
 # encoding: utf-8
-require "logstash/devutils/rspec/spec_helper"
 require "logstash/outputs/null"
+require "logstash/event"
+require "logstash/devutils/rspec/spec_helper"
 
 describe LogStash::Outputs::Null do
+  context "#register" do
+    it "should not raise an error" do
+      expect {subject.register }.not_to raise_error
+    end
+  end
+
+  context "#receive" do
+    let(:event) { LogStash::Event.new({ "message" => "foobar" }) }
+
+    it "should not raise an error" do
+      expect {subject.receive(event)  }.not_to raise_error
+    end
+  end
 end
 
